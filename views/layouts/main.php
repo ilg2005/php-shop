0ec478342c63708b5b4022030392f0fcc486ffbf
ilg2005 <igoldfarb10@gmail.com>
2020-11-01 13:37:52 +0300

    ЧПУ

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -38,9 +38,9 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-            ['label' => 'Hello', 'url' => ['/site/hello']],
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
+            ['label' => 'Hello', 'url' => ['/site/hello']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
