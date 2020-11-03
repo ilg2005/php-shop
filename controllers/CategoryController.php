476cd0f11789b99263cbf59824f176ec6891a8a5
ilg2005 <igoldfarb10@gmail.com>
2020-11-03 21:49:54 +0300

    подключение к БД, главная страница

---
+++ controllers/CategoryController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace app\controllers;
+
+
+use app\models\Good;
+use yii\web\Controller;
+
+class CategoryController extends Controller
+{
+
+    public function actionIndex() {
+        $goods = new Good();
+        $goods = $goods->getAllGoods();
+
+        return $this->render('index', compact('goods'));
+    }
+
+}
---
+++ models/Good.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace app\models;
+
+
+use yii\caching\Cache;
+use yii\db\ActiveRecord;
+use Yii;
+
+class Good extends ActiveRecord
+{
+    public function getAllGoods() {
+        $goods = Yii::$app->cache->get('goods');
+        if (!$goods) {
+            $goods = Good::find()->all();
+            Yii::$app->cache->set('goods', $goods, 60);
+        }
+        return $goods;
+    }
+
+    public static function tableName()
+    {
+        return 'good';
+    }
+}
