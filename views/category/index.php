e3043b1533170a5c8b5a571b0003aebb3b79c951
ilg2005 <igoldfarb10@gmail.com>
2020-11-05 13:31:03 +0300

    текущее состояние

--- views/category/index.php
+++ views/category/index.php
@@ -7,8 +7,8 @@ use app\widgets\MenuWidget;
     <div class="row">
         <?= MenuWidget::widget() ?>
     </div>
-    <?php if (Yii::$app->request->get('search')) : ?>
-        <h4 class="text-center">Результаты поиска "<?= $search ?>":</h4>
+    <?php if ($search) : ?>
+        <h4 class="text-center">Результаты поиска по запросу "<?= $search ?>":</h4>
     <?php endif; ?>
 
     <div class="row justify-content-center">
