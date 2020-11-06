ded310a1878227ed039052d4af92d57545aaf6d4
ilg2005 <igoldfarb10@gmail.com>
2020-11-06 14:18:54 +0300

    пустая корзина

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -53,6 +53,53 @@ AppAsset::register($this);
 <div class="modal fade" id="cart" tabindex="-1" aria-labelledby="cartLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
+            <div class="modal-header border-0">
+                <h2 class="modal-title" id="cartLabel">Корзина</h2>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+
+                <table class="table table-striped">
+
+                    <thead>
+                    <tr class="text-center">
+                        <th scope="col">Фото</th>
+                        <th scope="col">Наименование</th>
+                        <th scope="col">Количество</th>
+                        <th scope="col">Цена</th>
+                        <th scope="col"></th>
+                    </tr>
+                    </thead>
+
+                    <tbody>
+                    <tr class="text-center">
+                        <p class="font-weight-bold">Ваша корзина пока пуста!</p>
+                        <td style="vertical-align: middle" width="150"><img src="/img/<?= $good->img ?>" alt="--"></td>
+                        <td style="vertical-align: middle">--</td>
+                        <td style="vertical-align: middle">0</td>
+                        <td style="vertical-align: middle">0</td>
+                        <td class="delete" style="text-align: center; cursor: pointer; vertical-align: middle; color: red">
+                            <span>&#10006;</span></td>
+
+                    </tr>
+                    <tr style="border-top: 4px solid black">
+                        <td colspan="4">Всего товаров</td>
+                        <td class="total-quantity"> 0 </td>
+                    </tr>
+                    <tr>
+                        <td colspan="4">На сумму </td>
+                        <td style="font-weight: 700">0 рублей</td>
+                    </tr>
+                    </tbody>
+
+                </table>
+
+            </div>
+            <div class="modal-footer modal-buttons justify-content-around border-0 mb-3">
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Начать покупки</button>
+            </div>
         </div>
     </div>
 </div>
