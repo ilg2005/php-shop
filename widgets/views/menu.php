491fc3872150dedd58dccc7a7b7f3f442e000316
Igor <ilg2005@mail.ru>
2020-11-29 19:05:34 +0300

    текущее состояние

--- widgets/views/menu.php
+++ widgets/views/menu.php
@@ -1,8 +1,29 @@
+<?php
+
+use yii\helpers\Url;
+use yii\widgets\Menu;
+
+$items[] = [ 'label' => 'Всё меню', 'url' => '/category/index' ];
+
+foreach ($categories as $category) {
+    $items[] = ['label' => $category->browser_name, 'url' => Url::to(['category/index', 'catName' => $category->cat_name])];
+      }
+
+Menu::widget([
+    'options' => ['class' => 'nav nav-menu'],
+    'items' => $items,
+    'activeCssClass' => 'active',
+]);
+print_r($items);
+
+?>
+
 <nav class="nav nav-menu">
     <a class="nav-link active" href="/">Всё меню</a>
-    <?php use yii\helpers\Url;
 
-    foreach ($categories as $category) : ?>
-        <a class="nav-link" href="<?= Url::to(['category/index', 'catName' => $category->cat_name]) ?>"><?= $category->browser_name ?></a>
+
+    <?php foreach ($categories as $category) : ?>
+        <a class="nav-link"
+           href="<?= Url::to(['category/index', 'catName' => $category->cat_name]) ?>"><?= $category->browser_name ?></a>
     <?php endforeach; ?>
 </nav>
