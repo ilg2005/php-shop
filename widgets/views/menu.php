d7178318b24b2a7230cccd52d7a9cc0a84808d97
Igor <ilg2005@mail.ru>
2020-11-29 21:22:37 +0300

    текущее состояние

--- widgets/views/menu.php
+++ widgets/views/menu.php
@@ -9,6 +9,7 @@ foreach ($categories as $category) {
     $items[] = ['label' => $category->browser_name, 'url' => [Url::to(['category/index', 'catName' => $category->cat_name])]];
       }
 
+
 echo Menu::widget([
     'options' => ['class' => 'nav nav-menu'],
     'items' => $items,
