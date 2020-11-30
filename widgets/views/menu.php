5587da305a15b1ca33b156656cc7eef432460841
ilg2005 <igoldfarb10@gmail.com>
2020-11-30 11:55:21 +0300

    текущее состояние

--- widgets/views/menu.php
+++ widgets/views/menu.php
@@ -1,31 +1,19 @@
 <?php
 
 use yii\helpers\Url;
-use yii\widgets\Menu;
 
-$items[] = [ 'label' => 'Всё меню', 'url' => '/' ];
-
-foreach ($categories as $category) {
-    $items[] = ['label' => $category->browser_name, 'url' => [Url::to(['category/index', 'catName' => $category->cat_name])]];
-      }
-
-
-echo Menu::widget([
-    'options' => ['class' => 'nav nav-menu'],
-    'items' => $items,
-    'activeCssClass'=>'active',
-
-]);
 $currentUrl = Yii::$app->request->url;
-
 ?>
 
 <nav class="nav nav-menu">
-    <a class="nav-link <?=($currentUrl === '/') ? 'active' : '' ?>" href="/">Всё меню</a>
+    <a class="nav-link <?= ($currentUrl === '/') ? 'active' : '' ?>" href="/">Всё меню</a>
 
 
     <?php foreach ($categories as $category) : ?>
-        <a class="nav-link <?=($currentUrl === '/category/' . $category->cat_name) ? 'active' : '' ?>"
-           href="<?= Url::to(['category/index', 'catName' => $category->cat_name]) ?>"><?= $category->browser_name ?></a>
+        <a class="nav-link <?= ($currentUrl === '/category/' . $category->cat_name) ? 'active' : '' ?>"
+           href="<?= Url::to([
+               'category/index',
+               'catName' => $category->cat_name
+           ]) ?>"><?= $category->browser_name ?></a>
     <?php endforeach; ?>
 </nav>
