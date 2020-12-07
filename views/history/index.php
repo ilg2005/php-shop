6a169b1e3c170a5572f32f1a45f8361ca07a43d6
ilg2005 <igoldfarb10@gmail.com>
2020-12-07 13:29:43 +0300

    текущее состояние

--- views/history/index.php
+++ views/history/index.php
@@ -3,48 +3,55 @@
 use app\models\OrderGood;
 use sjaakp\bandoneon\Bandoneon;
 
-Bandoneon::begin() ?>
 
-<?php foreach ($orders as $order) : ?>
-    <h4>Заказ <strong><?= $order->id ?></strong> от <strong><?= $order->date ?></strong> <button type="button" class="btn btn-success btn-sm">Повторить</button></h4>
-    <div>
-        <table class="table table-striped">
+if (!count($orders)) : ?>
+    <h4> У Вас пока нет ни одного заказа </h4>
+<?php else: ?>
 
-            <thead>
-            <tr>
-                <th scope="col">Наименование</th>
-                <th scope="col">Количество</th>
-                <th scope="col">Цена</th>
-            </tr>
-            </thead>
+    <?php Bandoneon::begin() ?>
 
-            <tbody>
-            <?php $goods = OrderGood::find()->where(['order_id' => $order->id])->all(); ?>
-            <?php foreach ($goods as $id => $good): ?>
+    <?php foreach ($orders as $order) : ?>
+        <h4>Заказ <strong><?= $order->id ?></strong> от <strong><?= $order->date ?>&nbsp;&nbsp;</strong>
+            <button type="button" class="btn btn-success btn-sm">В корзину</button>
+        </h4>
+        <div>
+            <table class="table table-striped">
 
-                <tr data-id="<?= $id ?>">
-
-                    <td style="vertical-align: middle"><?= $good->name ?></td>
-                    <td style="vertical-align: middle"><?= $good->quantity ?> шт</td>
-                    <td style="vertical-align: middle"><?= $good->price * $good->quantity ?> рублей</td>
+                <thead>
+                <tr>
+                    <th scope="col">Наименование</th>
+                    <th scope="col">Количество</th>
+                    <th scope="col">Цена</th>
                 </tr>
-            <?php endforeach; ?>
-            <tr style="border-top: 4px solid black">
-                <td>Всего товаров</td>
-                <td class="text-left"><span class="total-quantity"><?= count($goods) ?></span> шт</td>
-                <td></td>
-            </tr>
-            <tr>
-                <td>На сумму</td>
-                <td></td>
-                <td class="text-left" style="font-weight: 700"><span
-                            class="total-price"><?= $order->sum ?></span> рублей
-                </td>
-            </tr>
-            </tbody>
+                </thead>
 
-        </table>
-    </div>
-<?php endforeach ?>
-<?php Bandoneon::end() ?>
+                <tbody>
+                <?php $goods = OrderGood::find()->where(['order_id' => $order->id])->all(); ?>
+                <?php foreach ($goods as $id => $good): ?>
 
+                    <tr data-id="<?= $id ?>">
+
+                        <td style="vertical-align: middle"><?= $good->name ?></td>
+                        <td style="vertical-align: middle"><?= $good->quantity ?> шт</td>
+                        <td style="vertical-align: middle"><?= $good->price * $good->quantity ?> рублей</td>
+                    </tr>
+                <?php endforeach; ?>
+                <tr style="border-top: 4px solid black">
+                    <td>Всего товаров</td>
+                    <td class="text-left"><span class="total-quantity"><?= count($goods) ?></span> шт</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>На сумму</td>
+                    <td></td>
+                    <td class="text-left" style="font-weight: 700"><span
+                                class="total-price"><?= $order->sum ?></span> рублей
+                    </td>
+                </tr>
+                </tbody>
+
+            </table>
+        </div>
+    <?php endforeach ?>
+    <?php Bandoneon::end() ?>
+<?php endif; ?>
