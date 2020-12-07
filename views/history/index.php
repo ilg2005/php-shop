995aa6055ed9315e7b5f500b6b139ddf377d6045
ilg2005 <igoldfarb10@gmail.com>
2020-12-07 18:24:11 +0300

    текущее состояние

--- views/history/index.php
+++ views/history/index.php
@@ -2,55 +2,60 @@
 
 use app\models\OrderGood;
 use sjaakp\bandoneon\Bandoneon;
+?>
 
+<div class="container mt-5">
+    <?php if (!count($orders)) : ?>
+        <h4> У Вас пока нет ни одного заказа </h4>
+    <?php else: ?>
+        <?php foreach ($orders as $order) : ?>
+            <div class="row mb-3 justify-content-center">
 
-if (!count($orders)) : ?>
-    <h4 class="mt-5"> У Вас пока нет ни одного заказа </h4>
-<?php else: ?>
-    <?php Bandoneon::begin() ?>
+                <?php Bandoneon::begin() ?>
+                <h4 class="mr-5">Заказ <strong><?= $order->id ?></strong> от <strong><?= $order->date ?></strong></h4>
+                <div>
+                    <table class="table table-striped">
 
-    <?php foreach ($orders as $order) : ?>
-        <h4 class="text-center">Заказ <strong><?= $order->id ?></strong> от <strong><?= $order->date ?>&nbsp;&nbsp;</strong>
-            <button type="button" class="btn btn-success btn-sm">В корзину</button>
-        </h4>
-        <div>
-            <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th scope="col">Наименование</th>
+                            <th scope="col">Количество</th>
+                            <th scope="col">Цена</th>
+                        </tr>
+                        </thead>
 
-                <thead>
-                <tr>
-                    <th scope="col">Наименование</th>
-                    <th scope="col">Количество</th>
-                    <th scope="col">Цена</th>
-                </tr>
-                </thead>
+                        <tbody>
+                        <?php $goods = OrderGood::find()->where(['order_id' => $order->id])->all(); ?>
+                        <?php foreach ($goods as $id => $good): ?>
 
-                <tbody>
-                <?php $goods = OrderGood::find()->where(['order_id' => $order->id])->all(); ?>
-                <?php foreach ($goods as $id => $good): ?>
+                            <tr data-id="<?= $id ?>">
 
-                    <tr data-id="<?= $id ?>">
+                                <td style="vertical-align: middle"><?= $good->name ?></td>
+                                <td style="vertical-align: middle"><?= $good->quantity ?> шт</td>
+                                <td style="vertical-align: middle"><?= $good->price * $good->quantity ?> рублей</td>
+                            </tr>
+                        <?php endforeach; ?>
+                        <tr style="border-top: 4px solid black">
+                            <td>Всего товаров</td>
+                            <td class="text-left"><span class="total-quantity"><?= count($goods) ?></span> шт</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>На сумму</td>
+                            <td></td>
+                            <td class="text-left" style="font-weight: 700"><span
+                                        class="total-price"><?= $order->sum ?></span> рублей
+                            </td>
+                        </tr>
+                        </tbody>
 
-                        <td style="vertical-align: middle"><?= $good->name ?></td>
-                        <td style="vertical-align: middle"><?= $good->quantity ?> шт</td>
-                        <td style="vertical-align: middle"><?= $good->price * $good->quantity ?> рублей</td>
-                    </tr>
-                <?php endforeach; ?>
-                <tr style="border-top: 4px solid black">
-                    <td>Всего товаров</td>
-                    <td class="text-left"><span class="total-quantity"><?= count($goods) ?></span> шт</td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>На сумму</td>
-                    <td></td>
-                    <td class="text-left" style="font-weight: 700"><span
-                                class="total-price"><?= $order->sum ?></span> рублей
-                    </td>
-                </tr>
-                </tbody>
+                    </table>
+                    <a href="/" class="btn btn-success btn-sm toCart btn-block">В корзину</a>
+                </div>
+                <?php Bandoneon::end() ?>
 
-            </table>
-        </div>
-    <?php endforeach ?>
-    <?php Bandoneon::end() ?>
-<?php endif; ?>
+            </div>
+        <?php endforeach ?>
+
+    <?php endif; ?>
+</div>
--- web/js/main.js
+++ web/js/main.js
@@ -106,3 +106,4 @@ modalContentElement.on('click', '#order', function (evt) {
     })*/
 
 });
+
