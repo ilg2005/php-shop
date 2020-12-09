8ce79b17a7eaa307d30c2cb5dc6beb4049e87d31
ilg2005 <igoldfarb10@gmail.com>
2020-12-09 14:44:37 +0300

    текущее состояние

--- controllers/AdminBehaviorsController.php
+++ controllers/AdminBehaviorsController.php
@@ -8,6 +8,8 @@ use Yii;
 use yii\filters\AccessControl;
 use yii\filters\VerbFilter;
 use yii\web\Controller;
+use yii\web\HttpException;
+use yii\web\NotFoundHttpException;
 
 class AdminBehaviorsController extends Controller
 {
@@ -24,6 +26,14 @@ class AdminBehaviorsController extends Controller
                         'actions' => ['index', 'create', 'view', 'update', 'delete', 'error'],
                         'matchCallback' => function () {
                             return Yii::$app->user->identity->is_admin;
+                        }
+                    ],
+                    [
+                        'allow' => false,
+                        'actions' => ['index', 'create', 'view', 'update', 'delete', 'error'],
+                        'roles' => ['?', '@'],
+                        'denyCallback' => function () {
+                            throw new HttpException(403, 'Доступ запрещен');
                         }
                     ],
                 ],
